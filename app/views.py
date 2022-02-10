f5d375659f1e18945b485234dd1048fdc845a7a2
Itawnya Walker <itawnya.walker@gmail.com>
2022-02-10 18:29:57 -0500

    Updated - Steps 4 and 5

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ This file creates your application.
 from shutil import register_unpack_format
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -25,9 +25,14 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-@app.route('/profile')
+@app.route('/profile/')
 def profile():
-    return render_template('profile.html')
+    date_joined = format_date_joined(2021,2,10)
+    return render_template('profile.html', joined= date_joined)
+
+def format_date_joined(y,m,d):
+    joined = datetime.date(y,m,d)
+    return joined.strftime("%B, %Y")   
 
 
 ###
